256112adeb5e019283b8ef8fc3c49a34394ce7db
PoYeh - 1evx <TPY@LAPTOP-36021G6K>
2025-01-07 16:37:17 +0800

    Resolve Data.java Conflict

--- src/main/java/managefile/Data.java
+++ src/main/java/managefile/Data.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
